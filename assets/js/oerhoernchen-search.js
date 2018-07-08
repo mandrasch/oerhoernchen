5adf7dc40127c5b365bc5fb8dc1b83b5ccf5889e
Matthias <info@matthias-andrasch.de>
2018-07-08 13:14:40 +0200

    better alt text for logos

--- assets/js/oerhoernchen-search.js
+++ assets/js/oerhoernchen-search.js
@@ -32,7 +32,7 @@ var generateList = function(list, selector) {
     var html = '<div class="col-xs-6 col-sm-3 col-md-3 nopad text-center">' +
       '<a href="https://'+object.url+'">'+
       '<label class="image-checkbox" data-providerlist="'+selector+'">' +
-      '<img class="img-fluid" src="'+OERHOERNCHEN_ASSET_IMG_URL+'provider_logos/' + object.image + '" alt="Logo ' + object.name + '" data-toggle="tooltip" data-placement="bottom" title="' + object.name + '" />' +
+      '<img class="img-fluid" src="'+OERHOERNCHEN_ASSET_IMG_URL+'provider_logos/' + object.image + '" alt="'+ object.name+'" data-toggle="tooltip" data-placement="bottom" title="' + object.name + '" />' +
       '<input type="checkbox" name="image" value="' + object.url + '"  data-title="' + object.name + '"  data-providerid="' + data_providerid + '" checked />' +
       '<i class="fa fa-check hidden"></i>' +
       '</label>' +
@@ -52,7 +52,7 @@ var generateListNotCompatible = function(list, selector) {
 
     var html = '<div class="col-xs-6 col-sm-3 col-md-3 nopad text-center">' +
       '<a href="https://' + object.url + '" target="_blank">' +
-      '<img class="img-fluid" src="'+OERHOERNCHEN_ASSET_IMG_URL+'provider_logos/' + object.image + '" alt="Logo ' + object.name + '" title="' + object.name + '" />' +
+      '<img class="img-fluid" src="'+OERHOERNCHEN_ASSET_IMG_URL+'provider_logos/' + object.image + '" alt="' + object.name + '" title="' + object.name + '" data-toggle="tooltip" data-placement="bottom" title="' + object.name + '" />' +
       '</a>' +
       '</div>';
 
@@ -475,7 +475,37 @@ $(document).ready(function() {
   /*
    * EVENTS
    */
-
+    /* LICENSE FILTER */
+    // listen for select change events
+  // license filter explanation
+  $('select[name="license-filter"], select[name="web-search-license-filter"], select[name="media-search-license-filter"]').change(function() {
+    var val = $(this).find(':selected').val();
+    var explanation_div = $(this).parent().parent().find('div.license-explanation:first');
+    //console.log('explanation_div',explanation_div,$(this).parent());
+    var explanation_header_div = $(this).parent().parent().parent();
+    console.log('explanation_header_div',explanation_header_div);
+    $('div', explanation_div).hide();
+    switch (val) {
+      case 'only-oer':
+        $('div[data-value="oer"]', explanation_div).show();
+        $("span.license-filter-heading", explanation_header_div).html("Lizenzfilter: CC0, CC BY / SA");
+        break;
+      case 'nc':
+        $('div[data-value="oer"]', explanation_div).show();
+        $('div[data-value="nc"]', explanation_div).show();
+        $("span.license-filter-heading", explanation_header_div).html("Lizenzfilter: CC0, CC BY / SA / NC");
+        break;
+      case 'nc-nd':
+        $('div[data-value="oer"]', explanation_div).show();
+        $('div[data-value="nc-nd"]', explanation_div).show();
+        $("span.license-filter-heading", explanation_header_div).html("Lizenzfilter: CC0, CC BY / SA / NC / ND");
+        break;
+      case 'no-filter':
+        $('div[data-value="no-filter"]', explanation_div).show();
+        $("span.license-filter-heading", explanation_header_div).html("Lizenzfilter: deaktiviert");
+        break;
+    }
+  }).change();
 
 
   // listen for submit events
