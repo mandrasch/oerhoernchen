fd214e72e3ffcef51eb2cd262aa6a5ca94836ee4
Matthias <info@matthias-andrasch.de>
2018-07-08 13:14:56 +0200

    move license filter to search js

--- assets/js/oerhoernchen.js
+++ assets/js/oerhoernchen.js
@@ -19,31 +19,4 @@ $(document).ready(function() {
     $(this).parent().parent().find('.card-body:first').toggle();
   });
 
-
-  /* LICENSE FILTER */
-    // listen for select change events
-  // license filter explanation
-  $('select[name="license-filter"], select[name="web-search-license-filter"], select[name="media-search-license-filter"]').change(function() {
-    var val = $(this).find(':selected').val();
-    var explanation_div = $(this).parent().parent().find('div.license-explanation:first');
-    console.log('explanation_div',explanation_div,$(this).parent());
-    $('div', explanation_div).hide();
-    switch (val) {
-      case 'only-oer':
-        $('div[data-value="oer"]', explanation_div).show();
-        break;
-      case 'nc':
-        $('div[data-value="oer"]', explanation_div).show();
-        $('div[data-value="nc"]', explanation_div).show();
-        break;
-      case 'nc-nd':
-        $('div[data-value="oer"]', explanation_div).show();
-        $('div[data-value="nc-nd"]', explanation_div).show();
-        break;
-      case 'no-filter':
-        $('div[data-value="no-filter"]', explanation_div).show();
-        break;
-    }
-  }).change();
-
 });
