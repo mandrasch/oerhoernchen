ec113f287f30b6a3c0caa917866b5becb3d43fca
Matthias <info@matthias-andrasch.de>
2018-07-13 12:34:03 +0200

    bugfix license filter events

--- assets/js/oerhoernchen.js
+++ assets/js/oerhoernchen.js
@@ -51,10 +51,11 @@ $(document).ready(function() {
   }
 
   // license filter explanation
-  $('select[name="license-filter"], select[name="web-search-license-filter"], select[name="media-search-license-filter"]').change(function() {
+  $('body').on('change','select[name="license-filter"], select[name="web-search-license-filter"], select[name="media-search-license-filter"]',function() {
+    console.log('license filter changed');
     var val = $(this).find(':selected').val();
     var explanation_div = $(this).parent().parent().find('div.license-explanation:first');
-    //console.log('explanation_div',explanation_div,$(this).parent());
+    console.log('explanation_div',explanation_div,$(this).parent());
     var explanation_header_div = $(this).parent().parent().parent();
     console.log('explanation_header_div', explanation_header_div);
     $('div', explanation_div).hide();
@@ -78,6 +79,7 @@ $(document).ready(function() {
         $("span.license-filter-heading", explanation_header_div).html("Lizenzfilter: deaktiviert");
         break;
     }
-  }).change();
+  });
+  $('select[name="license-filter"], select[name="web-search-license-filter"], select[name="media-search-license-filter"]').trigger('change');
 
 });
