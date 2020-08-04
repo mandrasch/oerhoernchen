e8f65fbd66849c358c1be45733662035de33dbb2
programmieraffe <info@matthias-andrasch.de>
2020-08-04 10:26:40 +0200

    Update oerhoernchen-search.php

--- templates/oerhoernchen-search.php
+++ templates/oerhoernchen-search.php
@@ -24,13 +24,13 @@
 
 
       <div class="alert alert-primary" role="alert"><small><i class="fa fa-bullhorn" aria-hidden="true"></i>
-        Neu: <a href="https://wirlernenonline.de/" target="_blank">wirlernenonline.de - Freie Bildung zum Mitmachen! <i class="fa fa-external-link" aria-hidden="true"></i></a></small>
+        Neue Suchmaschine: <a href="https://wirlernenonline.de/" target="_blank">wirlernenonline.de - Freie Bildung zum Mitmachen! <i class="fa fa-external-link" aria-hidden="true"></i></a></small>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
 
-      <div class="alert alert-dark" role="alert">
+      <div class="alert alert-dark" role="alert" style="display:none;">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
